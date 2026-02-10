049fb72ae1e8e797c6c1a00c4d003b617d4021af
BarryDeCicco <bdecicco2001@yahoo.com>
2026-02-10 17:19:44 -0500

    added comment on turning off shiny app

--- R/Create simple shiny app 'Hello, World'.R
+++ R/Create simple shiny app 'Hello, World'.R
@@ -15,6 +15,8 @@ server <- function(input, output) {
 shinyApp(ui, server)
 
 
+# turn off the above app
+# To turn off the Shiny app, you can simply stop the R session or press the "Stop" button in your R environment (such as RStudio) where the app is running. If you are running the app in a terminal, you can use Ctrl + C to stop it.
 
 
 
