6a63b87160002b1e4d7ced6a26e0be13ad047df0
BarryDeCicco <bdecicco2001@yahoo.com>
2026-02-11 16:29:18 -0500

    This works!

--- R/Using Ellmer.R
+++ R/Using Ellmer.R
@@ -4,8 +4,15 @@ install.packages("ellmer")
 library(ellmer)
 
 chat <- chat_openai(model = "gpt-4o")
-chat$chat("Hello from ellmer!")
+
+chat <- chat_anthropic()
 
 
+## chat$chat("Hello from ellmer!")
+
+chat$chat("please analyze the data set 'airquality'")
 
 
+#################### Interactive chat console ################
+
+live_console(chat)
