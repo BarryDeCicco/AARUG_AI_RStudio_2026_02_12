c9c48800520c697427cb565e16f681d8feaa53c0
BarryDeCicco <bdecicco2001@yahoo.com>
2026-02-12 19:23:06 -0500

    Imported after Claude made it during the presentation.

---
+++ R/Excel_Importer_ClaudeAI_First_During_Meeting.R
@@ -0,0 +1,10 @@
+
+# Q:  how do I list my data sets?
+# A:  You can list your data sets using the `list_datasets()` function from the `datasets` library in Python. Here's an example of how to do it:
+  # Q:  I need this in r
+# A:  In R, you can list your data sets using the `data()` function. This function will show you all the available data sets in your R environment. Here's how you can do it:
+# Load the datasets package
+library(datasets)
+# List all available data sets
+data()
+
